1d0c10a0e252c863deeabbbb9e90cf4a3139e31c
mrdranias <45687241+mrdranias@users.noreply.github.com>
2025-03-10 06:16:13 -0400

    Update up.sh

--- up.sh
+++ up.sh
@@ -5,8 +5,14 @@ docker compose up -d
 jupyter-nbextension install rise --py --sys-prefix
 jupyter-nbextension enable rise --py --sys-prefix
 
+# Start Jupyter Lab with fixed token 't3st'
+jupyter lab --ip=0.0.0.0 --no-browser --port=8877 --NotebookApp.token='test'
+
 
 code .
+
+# Deactivate the environment upon shutting down Jupyter (Ctrl+C)
+deactivate
 
 #source ./setenvvars.sh
 
